0ed85abdac123d18cc265621e260f4ad8f9a10d1
Bharathi Kannan <112495342+bharathi0811@users.noreply.github.com>
2023-07-17 23:01:37 +0530

    Add files via upload

---
+++ rotate_matrix.py
@@ -0,0 +1,12 @@
+R = int(input("Enter the number of rows:"))
+C = int(input("Enter the number of columns:"))
+matrix = []
+print("Enter the entries rowwise:")
+for i in range(R):
+    a =[]
+    for j in range(C):
+        a.append(int(input()))
+    matrix.append(a)
+matrix  = [list(row) for row in zip(*matrix)]
+matrix = [row[::-1] for row in matrix]
+print(matrix)
